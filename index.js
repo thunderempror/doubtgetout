5bf5f00e95528618adfa7602776311c91844d7ec
thunderempror <51469305+thunderempror@users.noreply.github.com>
2020-03-15 21:03:28 +0530

    javascript changes

--- index.js
+++ index.js
@@ -1,113 +1,88 @@
 
-document.querySelector(".post-question").addEventListener("click",function(){
-   
-     var question=document.querySelector("#question").value;
-     if(question=="")
-     {
-         alert("Please Enter Question! ");
-     }
-     
-     else{
+var i = 0;
+document.querySelector(".post-question").addEventListener("click", function () {
 
-     var newdiv=document.createElement("div");
-     newdiv.classList.add("posted-question");
-     
+    var question = document.querySelector("#question").value;
+    if (question == "") {
+        alert("Enter question");
+    }
+    else {
+        i++;
 
-
-     var answerbutton=document.createElement("button");
-     answerbutton.classList.add("btn");
-     answerbutton.classList.add("btn-success");
-     answerbutton.classList.add("typeanswer");
-     answerbutton.innerHTML="Answer"; 
-  
-
-
-     newdiv.innerHTML="<h3> Q: "+question+"</h3>"+"<br>";
-     newdiv.appendChild(answerbutton);
-
-
-     var rightcol=document.querySelector(".right-col");
-     rightcol.appendChild(newdiv);
-
-     //notification of number of question posted
-     document.querySelector(".ques-notify").innerHTML=document.querySelectorAll(".posted-question").length;
-    
-     
-
-
-
-     document.querySelector(".typeanswer").addEventListener("click",function(){
-          
-        newdiv.removeChild(answerbutton);
-        answerbutton=document.createElement("section");
-        answerbutton.classList.add("answer-post");
-       
-         answerbutton.innerHTML+="<h3> Answer : </h3>";
-         
-         answerbutton.innerHTML+="<textarea></textarea>";
-         answerbutton.lastChild.setAttribute("id","answer");
-           
-         answerbutton.innerHTML+="<br>"+"<br>";
-
-         answerbutton.innerHTML +="<button>Post Answer</button>";
-         answerbutton.lastChild.setAttribute("class","btn btn-info post-answer");
-                
-        newdiv.appendChild(answerbutton);
-
-
-       
-        document.querySelector(".post-answer").addEventListener("click",function(){
+        console.log(i);
         
-            alert("hey");
-            var answer=document.querySelector("#answer").value;
+        document.querySelector(".ques-notify").innerHTML=i;
+
+        var quesandanswer = document.createElement("div");
+        quesandanswer.setAttribute("class", "qandadiv" + i);
+
+        var questiondiv = document.createElement("div");
+        questiondiv.setAttribute("class", "question" + i);
+
+        questiondiv.innerHTML = "<h2> Q: " + question + "</h2>" + "<br>";
+        quesandanswer.appendChild(questiondiv);
+
+        var answerbutton = document.createElement("button");
+        answerbutton.setAttribute("class", "btn btn-info answerbutton answerbutton" + i);
+        answerbutton.setAttribute("onclick","answer("+i+")");
+        answerbutton.innerHTML = "Answer";
+
+        quesandanswer.appendChild(answerbutton);
+
+        document.querySelector(".right-col").appendChild(quesandanswer);
+
+        
+
+    }
+
+})
+
+
+function answer(identity)
+{   
+    document.querySelector(".qandadiv"+identity).lastChild.remove();
     
-            newdiv.removeChild(answerbutton);
-            
-            var answerdiv=document.createElement("div");
+    var answerdiv=document.createElement("div");
+    answerdiv.innerHTML+="<h3> Answer: </h3>";
     
-            answerdiv.innerHTML=answer;
-            console.log(answer);
-            newdiv.appendChild(answerdiv);
+    var textarea=document.createElement("textarea");
+    textarea.setAttribute("class","answer answer"+identity);
+    answerdiv.appendChild(textarea);
+
+     answerdiv.innerHTML+="<br><br>";
+    var answerpostbutton=document.createElement("button");
+    answerpostbutton.setAttribute("class","btn btn-success postanswer"+identity);
+    answerpostbutton.setAttribute("onclick","postanswer("+identity+")");
+    answerpostbutton.innerHTML="Post Answer";   
+    answerdiv.appendChild(answerpostbutton);
+
+    var qandadiv=document.querySelector(".qandadiv"+identity);
+    qandadiv.appendChild(answerdiv);
+
+}
+
+var answerposted=0;
+
+function postanswer(identity)
+{
+    var answer=document.querySelector(".answer"+identity).value;
     
+    if(answer=="")
+    {
+        alert("Please! Enter answer");
+    }
+    else{
       
-             
-    
-        });
-    
+        document.querySelector(".qandadiv"+identity).lastChild.remove();
 
+        var para=document.createElement("p")
+        para.innerHTML="Answer :"+answer;
 
+        document.querySelector(".qandadiv"+identity).appendChild(para);
+      
+        answerposted++;
 
+        document.querySelector(".ans-notify").innerHTML=answerposted;
 
-
-
-
-
-    });
-
-
-   
-    
-
-
-
-
-
-
-
-
-
-
-
-     }
-});
-
-
-
-
-
-
-
-
-
-
-// document.querySelector(".ans-notify").innerHTML=document.querySelectorAll(".question-answered").length;
+    }
+}
